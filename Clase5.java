df6b43a6042d54133e3379f48310318c06e93377
NathaliaReyes <104657695+NathaliaReyes@users.noreply.github.com>
2023-06-06 23:40:11 -0600

    Se creo el código para el ejercicio 2, sobre convertir ciertas Millas a Kilometros.

--- Clase5.java
+++ Clase5.java
@@ -2,21 +2,16 @@ import java.util.Scanner;
 
 public class Clase5 {
     public static void main(String[] Args){
-        //Calculadora de descuento:
-        //Pídele al usuario que ingrese el precio original de un producto.
-        //Pídele al usuario que ingrese el porcentaje de descuento.
-        //Calcula el precio final después de aplicar el descuento utilizando la fórmula: precioFinal = precioOriginal - (precioOriginal * descuento / 100).
-        //Muestra el precio final al usuario.
-        //Vamos a manejar todos los precios en dólares.
+        //Conversor de millas a kilómetros:
+        //Pídele al usuario que ingrese una distancia en millas.
+        //Convierte la distancia a kilómetros utilizando la fórmula: kilómetros = millas * 1.60934.
+        //Muestra la distancia en kilómetros al usuario.
 
         Scanner cargarDatos = new Scanner(System.in);
-        System.out.println("Bienvenid@s a nuestra tienda virtual! :)");
-        System.out.println("Ingrese el precio original del producto (en dólares)");
-        int precioProducto = cargarDatos.nextInt();
-        System.out.println("Ingrese el porcentaje de descuento aplicado a ese producto");
-        double porcentajeDescuento = cargarDatos.nextDouble();
-        double precioFinal = precioProducto - (precioProducto * porcentajeDescuento / 100);
-        System.out.println("El precio final del producto que seleccionó es: " + precioFinal + " dólares");
+        System.out.println("Ingrese una distancia en millas");
+        double millas = cargarDatos.nextDouble();
+        double distanciaEnKilometros = (millas * 1.60934);
+        System.out.println("La distancia en Kilometros es :" + distanciaEnKilometros + " Km");
 
     }
 }
