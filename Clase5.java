91c89278fd9ec967e19508e1056dd3dc10641315
NathaliaReyes <104657695+NathaliaReyes@users.noreply.github.com>
2023-06-06 23:47:42 -0600

    Se creo el código para el ejercicio 1, sobre convertir cierta edad de su mascota a años de perro.

--- Clase5.java
+++ Clase5.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class Clase5 {
     public static void main(String[] Args){
-        //Conversor de millas a kilómetros:
-        //Pídele al usuario que ingrese una distancia en millas.
-        //Convierte la distancia a kilómetros utilizando la fórmula: kilómetros = millas * 1.60934.
-        //Muestra la distancia en kilómetros al usuario.
+        //Calculadora de edad de perros:
+        //Pídele al usuario que ingrese la edad de su perro.
+        //Convierte esa edad a años de perro (se dice que 1 año humano equivale a 7 años de perro).
 
         Scanner cargarDatos = new Scanner(System.in);
-        System.out.println("Ingrese una distancia en millas");
-        double millas = cargarDatos.nextDouble();
-        double distanciaEnKilometros = (millas * 1.60934);
-        System.out.println("La distancia en Kilometros es :" + distanciaEnKilometros + " Km");
+        System.out.println("Ingrese la edad de su perro (en años)");
+        int edadPerro = cargarDatos.nextInt();
+        int edadConvertida = (edadPerro * 7);
+        System.out.println("La edad convertida a años perro es :" + edadConvertida + " años");
 
     }
 }
