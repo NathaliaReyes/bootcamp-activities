dae65f4e3600786739c6ee9bb7ccb5ed5bca0fa3
NathaliaReyes <104657695+NathaliaReyes@users.noreply.github.com>
2023-06-06 23:36:40 -0600

    Se creo el código para el ejercicio 3, sobre mostrar el precio de un producto después de aplicarle un descuento.

---
+++ Clase5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Clase5 {
+    public static void main(String[] Args){
+        //Calculadora de descuento:
+        //Pídele al usuario que ingrese el precio original de un producto.
+        //Pídele al usuario que ingrese el porcentaje de descuento.
+        //Calcula el precio final después de aplicar el descuento utilizando la fórmula: precioFinal = precioOriginal - (precioOriginal * descuento / 100).
+        //Muestra el precio final al usuario.
+        //Vamos a manejar todos los precios en dólares.
+
+        Scanner cargarDatos = new Scanner(System.in);
+        System.out.println("Bienvenid@s a nuestra tienda virtual! :)");
+        System.out.println("Ingrese el precio original del producto (en dólares)");
+        int precioProducto = cargarDatos.nextInt();
+        System.out.println("Ingrese el porcentaje de descuento aplicado a ese producto");
+        double porcentajeDescuento = cargarDatos.nextDouble();
+        double precioFinal = precioProducto - (precioProducto * porcentajeDescuento / 100);
+        System.out.println("El precio final del producto que seleccionó es: " + precioFinal + " dólares");
+
+    }
+}
